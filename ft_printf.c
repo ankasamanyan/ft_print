3e04d88cce7c2d4ae11315079fb43343eeafe806
Anahit K <anikakasamanyan@gmail.com>
2022-02-15 19:25:27 +0200

    if elses

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ankasamanyan <ankasamanyan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 13:40:48 by ankasamanya       #+#    #+#             */
-/*   Updated: 2022/02/15 15:49:19 by ankasamanya      ###   ########.fr       */
+/*   Updated: 2022/02/15 19:03:43 by ankasamanya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ static int	ft_check(char str, va_list *args)
 	count = 0;
 	if (str == '%')
 		count += ft_putchar('%');
-	if (str == 'c')
+	else if (str == 'c')
 		count += ft_putchar(va_arg(*args, int));
-	if (str == 's')
+	else if (str == 's')
 		count += ft_putstr(va_arg(*args, char *));
-	if (str == 'i' || str == 'd')
+	else if (str == 'i' || str == 'd')
 		count += ft_putnbr(va_arg(*args, int));
-	if (str == 'u')
+	else if (str == 'u')
 		count += ft_printu(va_arg(*args, unsigned int));
-	if (str == 'x')
+	else if (str == 'x')
 		count += ft_printx(va_arg(*args, unsigned int), 0);
-	if (str == 'X')
+	else if (str == 'X')
 		count += ft_printx(va_arg(*args, unsigned int), 1);
-	if (str == 'p')
+	else if (str == 'p')
 		count += ft_printp(va_arg(*args, void *));
 	return (count);
 }
